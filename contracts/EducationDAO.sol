9b4b3486710713998e79c207c980d67ea73a280c
sayf-ismail <63899527+sayf-ismail@users.noreply.github.com>
2022-08-14 21:57:59 +0800

    amend TrainingProposal struct, add createProposal, vote and createClass

--- contracts/EducationDAO.sol
+++ contracts/EducationDAO.sol
@@ -19,12 +19,15 @@ contract EducationDAO is AccessControl {
     bytes32 public constant INSTRUCTOR_ROLE = keccak256("INSTRUCTOR_ROLE");
     bytes32 public constant MEMBER_ROLE = keccak256("MEMBER_ROLE");
     
-    // Represents a training proposal
+    // Represents a training proposal 
     struct TrainingProposal{
 		uint256 id ;
 		string name;
-		uint256 votes;
+        address instructor;
+		uint256 voteCount;
 		uint256 end;
+        uint256 price;
+        uint8 minimumVotes;
 		bool executed;
 	}
     // Represents a class. Created once the training proposal has been approved.
@@ -34,12 +37,10 @@ contract EducationDAO is AccessControl {
         address payable instructor;
         bool started;
         bool ended;
-        uint256 minNumberOfStudents;
-        uint256 maxNumberOfStudents;
+        uint256 studentLimit;
         uint256 currentNumberOfStudents;
         uint256 price;
         uint256 reviewScore;
-
         
     }
     // Represents a student. User becomes a student once they join their first class?
@@ -51,8 +52,8 @@ contract EducationDAO is AccessControl {
     
 
     mapping(uint256 => TrainingProposal) public proposals;
-	mapping(address => mapping(uint256 => bool)) votes;
-    mapping(uint256 => Class) public classes;
+	mapping(address => mapping(uint256 => bool)) whoVoted;
+    mapping(uint256 => Class) classes;
     // User becomes a student once they join their first class?
     mapping(address => Member) members;
     // Holds the balances for each class, so at the end of the class we know how much money we need to transfer to the instructor. 
@@ -65,16 +66,13 @@ contract EducationDAO is AccessControl {
     uint256 memberFee;
     uint256 public memberCount;
     uint256 public classCount;
-
+	uint public voteTime;
+    uint public nextProposalId;
 
 
     constructor(uint256 _memberFee){
         owner = msg.sender;
-        
-        _setupRole(DEFAULT_ADMIN_ROLE, address(this));
-        //_setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        
-        
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         memberFee = _memberFee;
         memberCount = 0;
     }
@@ -87,38 +85,52 @@ contract EducationDAO is AccessControl {
         require(msg.value >= memberFee, "Not enough ETH to join the DAO");
         // Check that user is not already a member
         require(!hasRole(MEMBER_ROLE, msg.sender), "Caller is already a member!");
-        _grantRole(MEMBER_ROLE, msg.sender);
+        _setupRole(MEMBER_ROLE, msg.sender);
         Member storage s = members[msg.sender];
         s._address = msg.sender;
         memberCount += 1;
     }
 
     // Create a proposal
-    function createProposal(string memory _name, string memory _data) external onlyRole("MEMBER_ROLE"){
-        //TODO
+    function createProposal(string memory _name, string memory _data, uint256 _price) external onlyRole("MEMBER_ROLE"){
+        require(hasRole(MEMBER_ROLE, msg.sender), "Caller is not a member!");
+        voteCount = 0;
+
+        proposals[nextProposalId] = TrainingProposal(
+            nextProposalId,
+            _name,
+            msg.sender,
+            voteCount,
+            block.timestamp + voteTime,
+            _price,
+            2,
+            false
+        );
+        nextProposalId++;
     }
+
     //Vote on a proposal
-    function vote(uint256 proposalId) external onlyRole(MEMBER_ROLE){
+    function vote(uint256 proposalId) external onlyRole("MEMBER_ROLE"){
 		TrainingProposal storage proposal = proposals[proposalId];
-		require(votes[msg.sender][proposalId] == false, "User has already voted for this proposal");
+		require(whoVoted[msg.sender][proposalId] == false, "User has already voted for this proposal");
 		require(block.timestamp < proposal.end, "Voting period has ended");
-		votes[msg.sender][proposalId] = true;
-        // TODO
+		whoVoted[msg.sender][proposalId] = true;
+        proposal.voteCount+=1;
+        if (proposal.voteCount >= minimumVotes){
+            createClass(proposal.name, proposal.instructor, 20, proposal.price);
+        }
 	}
 
     // TODO
-    function createClass(string memory className, address payable _instructor, uint256 _minNumberOfStudents, uint256 _maxNumberOfStudents, uint256 _price) external {
-        
+    function createClass(string className, address payable _instructor, uint256 _studentLimit, uint256 _price) internal {
         uint256 nextClassId = classCount+1;
-        //Class storage c = 
-        classes[nextClassId] = Class ({
+        classes[nextClassId] = Class storage({
             id: nextClassId,
-            name: className,
+            name: nextClassId,
             instructor: _instructor,
             started: false,
             ended: false,
-            minNumberOfStudents: _minNumberOfStudents,
-            maxNumberOfStudents: _maxNumberOfStudents,
+            studentLimit: _studentLimit,
             currentNumberOfStudents: 0,
             price: _price,
             reviewScore: 0
@@ -127,21 +139,20 @@ contract EducationDAO is AccessControl {
     }
 
     // Start class -> Only the instructor should be able to start the class. How do we know the instructor's address?
-    function startClass(uint256 classId) external onlyRole(MEMBER_ROLE) verifyInstructor(classId) {
-        
+    function startClass(uint256 classId) external verifyInstructor(classId) {
+        // Check that msg.sender is the instructor for the given class
         Class storage c = classes[classId];
         // Check that the class has not started
         require(c.started == false, "This class has already started" );
         // Check that class has not ended
         require(c.ended == false, "This class has already ended" );
-        require(c.currentNumberOfStudents >= c.minNumberOfStudents , "Not enough students enrolled to start the class" );
         c.started = true;
-        _grantRole(INSTRUCTOR_ROLE, msg.sender);
+        _setupRole(INSTRUCTOR_ROLE, msg.sender);
     
     }
     // End class -> Only the instructor/owner should be able to end a class.
     //           -> Once the class has ended mint the certificate of completion (NFT).
-    function endClass(uint256 classId) external onlyRole(MEMBER_ROLE) verifyInstructor(classId) {
+    function endClass(uint256 classId) external verifyInstructor(classId) {
         // Check that msg.sender is the instructor for the given class
         // Check that msg.sender is the instructor for the given class
         Class storage c = classes[classId];
@@ -155,7 +166,7 @@ contract EducationDAO is AccessControl {
     
     // Join class (Payable) -> members would need to pay the full price to join a class.
     // Update the class currentNumberOfmembers and balance
-    function joinClass(uint256 classId) payable external onlyRole(MEMBER_ROLE){
+    function joinClass(uint256 classId) payable external onlyRole("MEMBER_ROLE"){
         Class storage c = classes[classId];
         // Check that the class has not started
         require(c.started == false, "This class has already started" );
@@ -164,7 +175,7 @@ contract EducationDAO is AccessControl {
         // CHeck if the class is full
         require(isClassFull(classId) == false, "Class is full");
         // If the user is already a student
-        if(hasRole(STUDENT_ROLE, msg.sender)){
+        if(getRoleMember(STUDENT_ROLE, msg.sender)){
             // Check that the user is not already enrolled in the class
             require(members[msg.sender].studentClasses[classId] == false, "You are already enrolled in this class");
             members[msg.sender].studentClasses[classId] = true;
@@ -177,25 +188,25 @@ contract EducationDAO is AccessControl {
             s.studentClasses[classId] = true;
             classBalances[classId] += msg.value;
             c.currentNumberOfStudents += 1;
-            _grantRole(STUDENT_ROLE, msg.sender);
+            _setupRole(STUDENT_ROLE, msg.sender);
         }
 
     }
 
     function isClassFull(uint256 classId) internal view returns (bool){
         Class storage c = classes[classId];
-        return !(c.currentNumberOfStudents < c.maxNumberOfStudents);
+        return !(c.currentNumberOfStudents < c.studentLimit);
     }
 
     // Withdraw from class. members are allowed to withdraw from a class as long as the class hasn't started. (For now)
     // TODO Refund policies
-    function withdrawFromClass(uint256 classId) external onlyRole(STUDENT_ROLE){
+    function withdrawFromClass(uint256 classId) external onlyRole("STUNDENT_ROLE"){
         Class storage c = classes[classId];
         require(c.started == false, "This class has already started" );
         Member storage s = members[msg.sender];
         require(s.studentClasses[classId] == true, "User is not enrolled in this class");
         // The following condition should never happen
-        require(classBalances[classId] >= c.price, "Class does not have enough balance");
+        require(classBalances[classId] > c.price, "Class does not have enough balance");
         s.studentClasses[classId] = false;
         classBalances[classId] = classBalances[classId] - c.price;
         studentBalances[msg.sender] = studentBalances[msg.sender] + c.price;
@@ -203,21 +214,21 @@ contract EducationDAO is AccessControl {
     }
 
     // students rate the class
-    function rateClass(uint256 classId, uint256 score) external onlyRole(STUDENT_ROLE) {
+    function rateClass(uint256 classId, uint256 score) external onlyRole("STUDENT_ROLE") {
         //TODO
     }
 
-    function setMemberFee(uint newMemberFee) external onlyRole(DEFAULT_ADMIN_ROLE){
+    function setMemberFee(uint newMemberFee) external onlyRole("DEFAULT_ADMIN_ROLE"){
         memberFee = newMemberFee;
     }
 
     // Admin adds the  role to the instructor address
-    function addInstructor(address instAddress) external onlyRole(DEFAULT_ADMIN_ROLE){
+    function addInstructor(address instAddress) external onlyRole("DEFAULT_ADMIN_ROLE"){
         //TODO
     }
 
     // members get their refunds
-    function getRefund() external onlyRole(STUDENT_ROLE){
+    function getRefund() external onlyRole("STUDENT_ROLE"){
         //TODO
     }
     //  Instructor calls the getPaid method once the class has ended. The instructor triggers the transfer
@@ -230,24 +241,8 @@ contract EducationDAO is AccessControl {
         //TODO
     }
 
-    function getClassesForStudent(address _member) external view returns (Class[] memory){
-        //TODO
-
-    }
-
-    function isMemberEnrolledInClass(address _member, uint256 classId) external view returns (bool){
-        
-        return members[_member].studentClasses[classId];
-    }
-
-    function getClass(uint256 _classId) external view returns (Class memory){
-        
-        return classes[_classId];
-    }
-    // TODO for testing only. delete later
-    function getClassBalance(uint256 _classId) external view returns (uint256){
-        
-        return classBalances[_classId];
+    function getMember(address _member) external{
+        //TODO 
     }
 
     // Modifier to verify that msg sender is the instructor for the given class.
@@ -256,6 +251,7 @@ contract EducationDAO is AccessControl {
         require(msg.sender == c.instructor, "Only the instructor can perform this activity");
         _;
     }
+
 
     
 }
